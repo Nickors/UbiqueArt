f1f4d391504dba2dd34dd542595b7a2c2aca3bcb
Niko <k-k-n-99@gmx.de>
2013-03-04 16:29:29 +0100

    FirstCommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UbiqueArt::Application.config.secret_token = '043a5b14381c5d242d667510fba5b8f33ede9a3f9a18e19deac6033b888d36a53e98d485c2449ec3470303770855dee20fb2965a0a1a2e34b8565ac45e512893'
